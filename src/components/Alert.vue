1fd97a8595d9adf68c0820c0d8ceb5c873db59c9
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2022-07-26 23:34:08 +0900

    [2022/07/26] alert

---
+++ src/components/Alert.vue
@@ -0,0 +1,57 @@
+<template id="subMain">
+    <v-alert 
+        color="red" 
+        width="300" 
+        max-height="200"
+        border
+        border-color="blue">
+        color="red" 
+        width="300" 
+        max-height="200"
+        border
+        border-color="blue"
+    </v-alert>
+
+    <v-alert 
+        width="300" 
+        max-height="200"
+        closable
+        close-label="dsada"
+        model-value
+        v-model="alert">
+        closable    
+        v-model="alert"
+    </v-alert>
+    <button @click="alert=true;" style="width:200px; margin-bottom:10px; border-bottom:1px solid black"> closable alert 다시 키기</button>
+
+    <v-alert 
+        width="300" 
+        max-height="200"
+        elevation="24">
+        elevation="24" *0~24의 숫자를 넣음    
+    </v-alert>
+
+    <v-alert 
+        width="300" 
+        max-height="200"
+        location="dasdsa">   
+        location
+    </v-alert>
+</template>
+elevation
+<script>
+export default {
+    name:'AlertComponent',
+    data() {
+        return {
+            alert:true
+        }
+    },
+}
+</script>
+
+<style scoped>
+.v-alert {
+    margin-bottom:20px;
+}
+</style>
